79c0cc646266b2508c674bf5a7f86776fd82189d
Susana Madriz <139632743+susanamadriz@users.noreply.github.com>
2025-11-04 17:34:17 +0100

    todo

---
+++ todo.c
@@ -0,0 +1,7 @@
+//TODO: 1. Parseo ( checkear repes, max - min int, numericos signos?)
+//TODO: 2. Passar numeros a lista( Creas nodo, añades a lista xinfinito)
+//TODO: 3. Hacer movimientos(sa sb pa pb ra rb rr rrr rra rrb)
+//TODO: 4. Buscas un algoritmo para ordenar (ksort, turco, chunks)
+//TODO: 5. Implementar algoritmo para ordenar
+//TODO: 6. Leaks
+//TODO: 7. Entregar
